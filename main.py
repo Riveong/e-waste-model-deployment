fd38e5ddcd878fab70683e836af78234a6fb190a
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2023-08-07 13:26:04 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import File, UploadFile
+from fastapi import *
 from google.cloud import storage
 import io
 from PIL import Image
@@ -9,34 +9,10 @@ import func as f
 from api_config import Tags
 import random
 from dotenv import load_dotenv
-import mysql.connector
 
+app = FastAPI()
 load_dotenv()
-
-print(os.getenv("cres"))
 port = int(os.getenv("PORT"))
-
-key_pi = os.getenv("cres")
-# GOOGLE_APPLICATION_CREDENTIALS = key_pi
-with open("service_account.json", "w") as file:
-    file.write(key_pi)
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./service_account.json"
-app = config.app
-form = str(random.randint(3, 3265792139879102375))
-bucketName = "techwaste"
-
-
-
-def storage_thingy(blobName, filePath, bucketName):
-    storageClient = storage.Client()
-    dir(storageClient)
-    bucket = storageClient.bucket(bucketName)
-    vars(bucket)
-    bucket = storageClient.get_bucket(bucketName)
-    blob = bucket.blob(blobName)
-    blob.upload_from_filename(filePath)
-
-    return blob
 
 
 @app.get("/")
@@ -51,18 +27,13 @@ async def predict(file: UploadFile = File(...)):
     if not extension:
         return "Image must be jpg or jpeg format!"
     contents = await file.read()
-    savedform = form
     image = Image.open(io.BytesIO(contents))
     image.save(file.filename)
     tf_image = f.preprocess_image(image)
     data_predict = f.predict_image(tf_image)
-    savedClass = data_predict
-    savedClass = str(savedClass[0]["Components Name"])
-    storage_thingy("predictSave/" + savedClass + savedform, file.filename, bucketName)
     os.remove(file.filename)
     return {"predictions": data_predict,
      "time_taken": f.timer(time),
-     "Image_Url": "https://storage.googleapis.com/techwaste/predictSave/"+savedClass+savedform
      }
 
 
